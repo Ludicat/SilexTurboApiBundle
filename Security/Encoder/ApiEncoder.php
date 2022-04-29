bfc09d4ffbcd13d3df6ae9c36f88ee8b279ec51c
Jihell <lemoine.joseph@gmail.com>
2022-04-29 14:49:23 +0200

    Update ApiEncoder.php

--- Security/Encoder/ApiEncoder.php
+++ Security/Encoder/ApiEncoder.php
@@ -1,27 +1,50 @@
 <?php
+/**
+ * @licence Proprietary
+ */
+namespace Auth\HttpApiBundle\Security\Encoder;
 
-namespace Devolicious\SilexTurboApiBundle\Security\Encoder;
+use Devolicious\SilexTurboApiBundle\Security\Encoder\ApiEncoder as BaseEncoder;
 
 /**
-* @author ruud
-*/
-class ApiEncoder
+ * Class ApiEncoder
+ *
+ * @author Joseph LEMOINE <j.lemoine@ludi.cat>
+ */
+class ApiEncoder extends BaseEncoder
 {
-    private $salt;
-
-    public function hash($username, $apiKey)
+    /**
+     * @param string $username
+     * @param string $apiKey
+     * @param int $days
+     *
+     * @return string
+     */
+    public function hash($username, $apiKey, int $days = 0)
     {
-        $today = new \DateTime();
-
-        return md5(strtolower($username).$today->format('d-m-Y').$apiKey);
+        $day = new \DateTime();
+        if (0 !== $days) {
+            $day->modify(sprintf('%d day', $day));
+        }
+        
+        return md5(strtolower($username).$day->format('d-m-Y').$apiKey);
     }
 
+    /**
+     * Check on day before and after the current one to avoid timezone issues
+     * 
+     * @param string $hash
+     * @param string $username
+     * @param string $apiKey
+     *
+     * @return bool
+     */
     public function isHashValid($hash, $username, $apiKey)
     {
-        if ($hash == $this->hash($username, $apiKey)) {
-            return true;
-        }
-
-        return false;
+        return in_array($hash, [
+            $this->hash($username, $apiKey, -1),
+            $this->hash($username, $apiKey),
+            $this->hash($username, $apiKey, 1),
+        ]);
     }
 }
